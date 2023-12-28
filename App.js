5b27446b9b64179563d909f0eddb2b63dc3354b9
gustavo de assis <gustavo.siqueira.421@gmail.com>
2023-12-28 16:44:03 -0300

    feat: footer buttons component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import PageHeader from "./components/PageHeader";
+import ZapButton from "./components/ZapButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <PageHeader />
+      {/*  <ZapButton
+        color="#777"
+        label="teste"
+        onPress={() => {
+          alert("oi!");
+        }}
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/ZapButton.js
@@ -0,0 +1,35 @@
+import { Pressable, StyleSheet, View, Text } from "react-native";
+
+export default function ZapButton({ label, color, onPress }) {
+  return (
+    <View sytle={styles.buttonContainer}>
+      <Pressable
+        style={[styles.button, { backgroundColor: color }]}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 100,
+    height: 70,
+    marginHorizontal: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderRadius: 5,
+    width: 90,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonLabel: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
